153a270888cf07644bbe7b3241ae9e3e1dfd630e
Ahmed Fahad <amdfad@Fahads-MBP.local>
2023-04-02 06:32:14 +0600

    Chatbot Demo

---
+++ chatbot.py
@@ -0,0 +1,41 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+openai.api_key = st.secrets['pass']
+
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=1024,
+        n = 1,
+    )
+    return response.choices[0].text
+
+st.title("🤖BotBD🤖")
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text 
+
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1,-1,-1):
+        message(st.session_state['generated'][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
